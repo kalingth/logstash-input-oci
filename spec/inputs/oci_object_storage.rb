2f3d558e5cd7d9ff8d0c943ffbdea02ad784e8c1
Kalingth <wallace13@hotmail.com.br>
2023-07-23 15:36:19 +0000

    name changes

--- spec/inputs/oci_object_storage.rb
+++ spec/inputs/oci_object_storage.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/devutils/rspec/shared_examples"
-require "logstash/inputs/object_storage"
+require "logstash/inputs/oci_object_storage"
 
-describe LogStash::Inputs::ObjectStorage do
+describe LogStash::Inputs::OciObjectStorage do
 
   it_behaves_like "an interruptible input plugin" do
     let(:config) { { "interval" => 100 } }
