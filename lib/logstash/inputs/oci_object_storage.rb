b06d5290a20d2608477059403a81bf43e3333e56
Kalingth <wallace13@hotmail.com.br>
2023-07-25 03:31:21 +0000

    Document

--- lib/logstash/inputs/oci_object_storage.rb
+++ lib/logstash/inputs/oci_object_storage.rb
@@ -16,6 +16,11 @@ require 'logstash/inputs/base'
 class ObjectStorageGetter
   attr_reader :sincedb_time
 
+  # Archive an object in the Oracle Cloud Infrastructure (OCI) Object Storage.
+  # The archiving is performed based on the configuration specified in the `@archieve_after_read` attribute
+  # of the `ObjectStorageGetter` class.
+  #
+  # @param object [Object] The object containing metadata information. It represents a file in the OCI Object Storage.
   def archieve_object(object)
     return unless @archieve_after_read
 
@@ -26,6 +31,12 @@ class ObjectStorageGetter
     @client.update_object_storage_tier(@namespace, @bucket_name, parmeters)
   end
 
+  # Decodes the data of the given object using gzip decompression if the file has a `.gz` or `.gzip` extension.
+  # If the object's name does not match the pattern for gzip files, the data is returned as-is without decompression.
+  #
+  # @param object [Object] The object containing metadata information. It represents a file in the OCI Object Storage.
+  # @param response [Object] The response object containing the data of the object retrieved from the Object Storage.
+  # @return [String] The decoded data as a String.
   def decode_gzip_file(object, response)
     return response.data.to_s unless object.name.match(/gz(ip)?/i)
 
@@ -34,6 +45,11 @@ class ObjectStorageGetter
     raw.read
   end
 
+  # Process the raw data using the specified Logstash codec and add metadata from the object to each event.
+  # After, includes this event on Logstash Queue.
+  #
+  # @param raw_data [String] The raw data to be processed. It represents the content of a file.
+  # @param object [Object] The object containing metadata information. It represents a file in the OCI Object Storage.
   def process_data(raw_data, object)
     meta = JSON.parse object.to_hash.to_json
     @codec.decode(raw_data) do |event|
@@ -42,6 +58,9 @@ class ObjectStorageGetter
     end
   end
 
+  # Download and process the data of the given object from the OCI Object Storage bucket.
+  #
+  # @param object [Object] The object containing metadata information. It represents a file in the OCI Object Storage.
   def download_file(object)
     response = @client.get_object(
       @namespace,
@@ -55,6 +74,10 @@ class ObjectStorageGetter
     @logger.warn("The file #{object.name} cannot be downloaded: #{e} => #{object.to_hash}")
   end
 
+  # Check whether the given object should be skipped or processed based on specific conditions.
+  #
+  # @param object [Object] The object containing metadata information. It represents a file in the OCI Object Storage.
+  # @param normalized_time [Time] The normalized time of the object's modification timestamp used for comparison.
   def skip_file?(object, normalized_time)
     if ['Archive'].include? object.storage_tier
       true
@@ -67,6 +90,11 @@ class ObjectStorageGetter
     end
   end
 
+  # Download and process filtered files from the OCI Object Storage bucket.
+  # Download and processing of objects are performed concurrently using a thread pool, with the number of threads
+  # specified by the `@threads` attribute of the `ObjectStorageGetter` class.
+  #
+  # @param buffer [Array] An array containing objects to be downloaded and processed. Each object represents a file.
   def download_filtered_files(buffer)
     time_buffer = []
     pool = Thread.pool(@threads)
@@ -82,6 +110,11 @@ class ObjectStorageGetter
     @sincedb_time = time_buffer.max unless time_buffer.empty? && (@filter_strategy == 'sincedb')
   end
 
+  # Retrieve files from the OCI Object Storage bucket recursively based on specified parameters.
+  #
+  # @param parameters [Hash] A hash containing parameters for the OCI Object Storage API request.
+  # @param buffer [Array] An optional array that accumulates the retrieved objects. Each object represents a file.
+  # @return [Array] An array containing the retrieved objects after the recursive retrieval process.
   def retrieve_files_recursive(parameters, buffer = [])
     response = @client.list_objects(@namespace, @bucket_name, parameters)
     buffer.push(*response.data.objects)
@@ -96,6 +129,9 @@ class ObjectStorageGetter
     retrieve_files_recursive(parameters, buffer)
   end
 
+  # Retrieve files from the OCI Object Storage bucket based on the specified prefix.
+  #
+  # @param prefix [String] An optional parameter to filter files based on that name prefix. Default is an empty string.
   def retrieve_files(prefix = '')
     parameters = {
       prefix: prefix,
@@ -106,6 +142,20 @@ class ObjectStorageGetter
     download_filtered_files buffer
   end
 
+  # Initialize a new instance of the ObjectStorageGetter class with the given parameters.
+  #
+  # Parameters:
+  # @param parameters [Hash] A hash containing configuration parameters for the ObjectStorageGetter instance.
+  #   - :queue [LogStash::Util::WrappedSynchronousQueue] The queue to which the processed events will be sent.
+  #   - :codec [Codec] The codec used to decode data from the retrieved objects in the OCI Object Storage bucket.
+  #   - :namespace [String] The OCI Object Storage namespace associated with the bucket.
+  #   - :bucket_name [String] The name of the OCI Object Storage bucket.
+  #   - :credentials [OCI::ConfigFile::Config] The OCI configuration used for authentication.
+  #   - :archieve_after_read [Boolean] A flag indicating whether the retrieved objects should be archived after read.
+  #   - :filter_strategy [String] Strategy used for filtering files during retrieval. Can be `archive` or `sincedb`.
+  #   - :sincedb_time [Time] Last modified timestamp recorded in the sincedb for filtering based on the 'sincedb'.
+  #   - :logger [Logger] Logger instance used to log information, warnings, and errors during the retrieval process.
+  #   - :threads [Integer] The number of threads used for concurrent processing of the retrieved objects.
   def initialize(parameters)
     @queue = parameters[:queue]
     @codec = parameters[:codec]
@@ -121,7 +171,16 @@ class ObjectStorageGetter
   end
 end
 
+# The LogStash module provides a namespace for all Logstash-related functionalities and classes.
+# It encapsulates the components required for data ingestion, processing, and output in the Logstash pipeline.
+# The module contains various submodules and classes that define inputs, filters, codecs, and outputs.
+# These components work together to collect, filter, and transform data from various sources, enabling data
+# ingestion into the Logstash pipeline.
 module LogStash
+  # The LogStash::Inputs module is a submodule of the LogStash module, specifically responsible for handling data
+  # input in the Logstash pipeline. It provides a namespace for all input-related functionalities, including various
+  # input plugins that enable Logstash to collect data from different sources. Each input plugin represents a specific
+  # data source or input type, such as files, databases, HTTP endpoints, message queues, and more.
   module Inputs
     # The OciObjectStorage class is a Logstash input plugin that enables Logstash to read data
     # from Oracle Cloud Infrastructure (OCI) Object Storage.
@@ -143,6 +202,9 @@ module LogStash
       config :interval, validate: :number, default: 30
       config :sincedb_path, validate: :string, default: nil
 
+      # Get the folder path for the sincedb file used by the OCI Object Storage input plugin in Logstash.
+      #
+      # @return [String] The folder path for the sincedb file specific to the OCI Object Storage input plugin.
       def sincedb_folder
         logstash_path = LogStash::SETTINGS.get_value('path.data')
         dir = File.join(logstash_path, 'plugins', 'inputs', 'oci_object_storage')
@@ -150,6 +212,9 @@ module LogStash
         dir
       end
 
+      # Get the full path to the sincedb file for the OCI Object Storage input plugin in Logstash.
+      #
+      # @return [String] The full path to the sincedb file specific to the OCI Object Storage input plugin.
       def sincedb_file
         digest = Digest::MD5.hexdigest("#{@namespace}+#{@bucket_name}+#{@prefix}")
         path = File.join(sincedb_folder, "sincedb_#{digest}")
@@ -163,6 +228,9 @@ module LogStash
         path
       end
 
+      # The register method is a lifecycle method in the OCIObjectStorage input plugin for Logstash.
+      # It is called once during the plugin initialization phase and is responsible for setting up sincedb
+      # functionality when the 'sincedb' filter strategy is enabled.
       def register
         return unless filter_strategy == 'sincedb'
 
@@ -177,6 +245,11 @@ module LogStash
         @sincedb_time = @sincedb.read
       end
 
+      # The run method is a lifecycle method in the OCIObjectStorage input plugin for Logstash.
+      # It is responsible for the main execution of the OCIObjectStorage input plugin and the retrieval and
+      # processing of files from the OCI Object Storage bucket.
+      #
+      # @param queue [LogStash::Util::WrappedSynchronousQueue] The queue to which the processed events will be sent.
       def run(queue)
         @logger.info("Loading config file: #{@config_path}")
         parameters = {
@@ -191,29 +264,47 @@ module LogStash
           threads: @threads,
           logger: @logger
         }
-        client = ObjectStorageGetter.new(parameters)
+        @client = ObjectStorageGetter.new(parameters)
         until stop?
-          client.retrieve_files
-          @sincedb.write(client.sincedb_time) if filter_strategy == 'sincedb'
+          @client.retrieve_files
+          @sincedb.write(@client.sincedb_time) if filter_strategy == 'sincedb'
           Stud.stoppable_sleep(@interval) { stop? }
         end
       end
 
+      # The stop method is a lifecycle method in the OCIObjectStorage input plugin for Logstash.
+      # It is responsible for stopping the execution of the OCIObjectStorage input plugin and performing
+      # any necessary cleanup tasks before exiting.
+      #
+      # @return [Boolean] Always returns true to signal that the Logstash pipeline should be stopped.
       def stop
+        @sincedb.write(@client.sincedb_time) if filter_strategy == 'sincedb'
         true
       end
     end
   end
 end
 
+# The SinceDB module provides a namespace for classes related to managing since database (sincedb) functionality.
+# The sincedb is a feature used to keep track of the progress in reading and processing files, ensuring that
+# Logstash resumes where it left off in case of interruptions or restarts. The module contains classes and methods
+# that handle reading and writing sincedb files, storing information about the last processed position in a file,
+# and updating it as new data is ingested.
 module SinceDB
-  # Foo
+  # The SinceDB::File class represents a sincedb file used to track the progress of reading and processing files
+  # in Logstash. It provides methods to read and write sincedb information, which helps to maintain data integrity
+  # and support incremental data processing for file-based data sources in Logstash.
   class File
+    # Constructor method to initialize a new instance of SinceDB::File.
+    #
+    # @param file [String] The path to the sincedb file to be managed by this instance.
     def initialize(file)
       @sincedb_path = file
     end
 
-    # @return [Time]
+    # Read the sincedb file and return the last processed timestamp as a Time object.
+    #
+    # @return [Time] The last processed timestamp recorded in the sincedb file, represented as a Time object.
     def read
       if ::File.exist?(@sincedb_path)
         content = ::File.read(@sincedb_path).chomp.strip
@@ -224,6 +315,9 @@ module SinceDB
       end
     end
 
+    # Write the provided timestamp to the sincedb file.
+    #
+    # @param since [Time] An optional parameter representing the timestamp. If not provided, Time.now is used.
     def write(since = nil)
       since = Time.now if since.nil?
       ::File.open(@sincedb_path, 'w') { |file| file.write(since.to_s) }
